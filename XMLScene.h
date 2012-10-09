4b7aa6614f7553684271d00007fb54af0b771e97
Tiago Vila Verde <tiagoandrevilaverde@gmail.com>
2012-10-09 16:23:37 +0100

    xmlscene.h

--- XMLScene.h
+++ XMLScene.h
@@ -2,6 +2,11 @@
 #define _XMLSCENE_H_
 
 #include "tinyxml.h"
+#include "GlobalValues.h"
+#include "CameraValues.h"
+#include "AppearanceValues.h"
+#include "LightValues.h"
+#include "SceneGraph.h"
 #include <set>
 
 using namespace std;
@@ -13,12 +18,14 @@ public:
     ~XMLScene();
 
     static TiXmlElement *findChildByAttribute(TiXmlElement *parent, const char * attr, const char *val);
-	void parsingCycle(TiXmlElement* elem);
+
+	void initParser(TiXmlElement* elem);
+	void parsingCycle(TiXmlElement* elem, SceneValues val);
+	void attributeParser(SceneValues val, TiXmlElement* elem);
 
 protected:
 
 	set <TiXmlElement*> elements;
-
 
     TiXmlDocument* doc;
 
@@ -29,6 +36,11 @@ protected:
     TiXmlElement* leavesElement;
     TiXmlElement* nodesElement;
     TiXmlElement* graphElement;
+
+	GlobalValues gv;
+	CameraValues cv;
+	LightValues lv;
+//	AppearanceValues av;
 };
 
 #endif
