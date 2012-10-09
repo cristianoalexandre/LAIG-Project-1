98cc708d326d36cf33e65a8aa75c36fceb8450ec
Tiago Vila Verde <tiagoandrevilaverde@gmail.com>
2012-10-09 12:04:28 +0100

    Construtor do XMLScene v1.0
    Parametros do ParsingCycle sem a referência do set<>;

--- XMLScene.cpp
+++ XMLScene.cpp
@@ -3,244 +3,186 @@
 #include <iostream>
 #include <vector>
 
-void parsingCycle(TiXmlElement* elem)
-{
 
-    TiXmlElement* child = elem->FirstChildElement();
-    if (child == NULL)
-    {
-        printf("no more children\n");
-        return;
-    }
 
-    while (child != NULL)
-    {
+void parsingCycle(TiXmlElement* elem/*, &set<*/){
 
-        char* name = (char*) child->Value();
-        printf("parsing %s element:\n", name);
 
-        TiXmlAttribute* attr = child->FirstAttribute();
-        while (attr != NULL)
-        {
+	
 
-            char* tmp = (char*) attr->Value();
-            char* attrName = (char*) attr->Name();
-            printf("attribute %s = %s\n", attrName, tmp);
-            attr = attr->Next();
-        }
 
-        parsingCycle(child);
-        child = child->NextSiblingElement();
-    }
-    return;
+
+
+	char* name = (char*) elem->Value();
+	printf("parsing %s element:\n", name);
+
+	
+	TiXmlAttribute* attr = elem->FirstAttribute();
+	while(attr != NULL){
+
+		char* tmp = (char*) attr->Value();
+		char* attrName = (char*) attr->Name();
+		printf("attribute %s = %s\n", attrName, tmp);
+		attr = attr->Next();
+
+	}
+
+
+	TiXmlElement* child = elem->FirstChildElement();
+	if(child == NULL){
+		printf("no more children\n");
+		return;
+	}
+
+	while(child != NULL){
+
+		parsingCycle(child);
+		child = child->NextSiblingElement();
+	}
+	return;
 }
 
-void queryStringValue(TiXmlElement* elem, char* attr, char* value)
-{
-
-    value = NULL;
-    value = (char*) elem->Attribute(attr);
-
-    if (value != NULL)
-    {
-        printf("%s: %s\n", attr, value);
-
-    }
-    else
-    {
-        printf("%s not found\n", attr);
-    }
-
-}
-
-void queryBoolValue(TiXmlElement* elem, char* attr, bool* value)
-{
-
-    char* tmp = NULL;
-    tmp = (char*) elem->Attribute(attr);
-
-    if (tmp != NULL)
-    {
-        if (tmp == "true")
-        {
-            *value = true;
-        }
-        else
-        {
-            *value = false;
-        }
-
-        printf("%s: %s\n", attr, tmp);
-
-    }
-    else
-    {
-        printf("%s not found\n", attr);
-    }
-
-}
 
 XMLScene::XMLScene(char *filename)
 {
 
-    vector<char*> attributes;
-    vector<char*> names;
+	vector<char*> attributes;
+	vector<char*> names;
 
-    doc = new TiXmlDocument(filename);
-    bool loadOkay = doc->LoadFile();
+	doc = new TiXmlDocument(filename);
+	bool loadOkay = doc->LoadFile();
 
-    if (!loadOkay)
-    {
-        printf("Could not load file '%s'. Error='%s'. Exiting.\n", filename, doc->ErrorDesc());
-        exit(1);
-    }
+	if (!loadOkay) {
+		printf("Could not load file '%s'. Error='%s'. Exiting.\n", filename, doc->ErrorDesc());
+		exit(1);
+	}
 
-    TiXmlElement* lsfElement = doc->FirstChildElement("lsf");
+	TiXmlElement* lsfElement = doc->FirstChildElement("lsf");
 
-    if (lsfElement == NULL)
-    {
-        printf("Main lsf block element not found! Exiting!\n");
-        exit(1);
-    }
+	if (lsfElement == NULL) {
+		printf("Main lsf block element not found! Exiting!\n");
+		exit(1);
+	}
 
-    globalsElement = lsfElement->FirstChildElement("globals");
-    camerasElement = lsfElement->FirstChildElement("cameras");
-    lightningElement = lsfElement->FirstChildElement("lightning");
-    appearancesElement = lsfElement->FirstChildElement("appearances");
-    leavesElement = lsfElement->FirstChildElement("leaves");
-    nodesElement = lsfElement->FirstChildElement("nodes");
-    graphElement = lsfElement->FirstChildElement("graph");
+	globalsElement = lsfElement->FirstChildElement("globals");
+	camerasElement = lsfElement->FirstChildElement("cameras");
+	lightningElement = lsfElement->FirstChildElement("lightning");
+	appearancesElement = lsfElement->FirstChildElement("appearances");
+	leavesElement = lsfElement->FirstChildElement("leaves");
+	nodesElement = lsfElement->FirstChildElement("nodes");
+	graphElement = lsfElement->FirstChildElement("graph");
 
-
-
-    if (globalsElement == NULL)
-    {
-        printf("Globals block not found!\n");
-
-    }
-    else
-    {
-        printf("Processing Globals:\n");
-
-        parsingCycle(globalsElement);
-    }
-
-    if (camerasElement == NULL)
-    {
-        printf("cameras block not found\n");
-    }
-    else
-    {
-        char* camIni;
-        queryStringValue(camerasElement, "initial", camIni);
+	this->elements.insert(globalsElement);
+	this->elements.insert(camerasElement);
+	this->elements.insert(lightningElement);
+	this->elements.insert(appearancesElement);
+	this->elements.insert(graphElement);
+	this->elements.insert(nodesElement);
+	this->elements.insert(leavesElement);
 
 
-        parsingCycle(camerasElement);
-    }
+	if (globalsElement == NULL){
+		printf("Globals block not found!\n");
 
-    if (lightningElement == NULL)
-    {
-        printf("lightning block not found\n");
-    }
-    else
-    {
-        bool doublesided, local, enabled;
+	} else {
+		parsingCycle(globalsElement);
+	}
 
-        queryBoolValue(lightningElement, "doublesided", &doublesided);
-        queryBoolValue(lightningElement, "local", &local);
-        queryBoolValue(lightningElement, "enabled", &enabled);
+	if (camerasElement == NULL) {
+		printf("cameras block not found\n");
+	} else {
 
-        parsingCycle(lightningElement);
-    }
+		parsingCycle(camerasElement);
+	}
 
-    if (appearancesElement == NULL)
-    {
-        printf("appearances block not found\n");
-    }
-    else
-    {
-        parsingCycle(appearancesElement);
-    }
+	if(lightningElement == NULL){
+		printf("lightning block not found\n");
+	}else{
+		parsingCycle(lightningElement);
+	}
+
+	if(appearancesElement == NULL){
+		printf("appearances block not found\n");
+	}else{
+		parsingCycle(appearancesElement);
+	}
 
 
-    // graph section
-    if (graphElement == NULL)
-        printf("Graph block not found!\n");
-    else
-    {
-        char *prefix = "  -";
-        TiXmlElement *node = graphElement->FirstChildElement();
+	// graph section
+	if (graphElement == NULL)
+		printf("Graph block not found!\n");
+	else {
 
-        while (node)
-        {
-            printf("Node id '%s' - Descendants:\n", node->Attribute("id"));
-            TiXmlElement *child = node->FirstChildElement();
-            while (child)
-            {
-                if (strcmp(child->Value(), "Node") == 0)
-                {
-                    // access node data by searching for its id in the nodes section
+		parsingCycle(graphElement);
+	}
+		/*char *prefix = "  -";
+		TiXmlElement *node = graphElement->FirstChildElement();
 
-                    TiXmlElement *noderef = findChildByAttribute(nodesElement, "id", child->Attribute("id"));
+		while (node) {
+			printf("Node id '%s' - Descendants:\n", node->Attribute("id"));
+			TiXmlElement *child = node->FirstChildElement();
+			while (child) {
+				if (strcmp(child->Value(), "Node") == 0) {
+					// access node data by searching for its id in the nodes section
 
-                    if (noderef)
-                    {
-                        // print id
-                        printf("  - Node id: '%s'\n", child->Attribute("id"));
+					TiXmlElement *noderef = findChildByAttribute(nodesElement, "id", child->Attribute("id"));
 
-                        // prints some of the data
-                        printf("    - Material id: '%s' \n", noderef->FirstChildElement("material")->Attribute("id"));
-                        printf("    - Texture id: '%s' \n", noderef->FirstChildElement("texture")->Attribute("id"));
+					if (noderef) {
+						// print id
+						printf("  - Node id: '%s'\n", child->Attribute("id"));
 
-                        // repeat for other leaf details
-                    }
-                    else
-                        printf("  - Node id: '%s': NOT FOUND IN THE NODES SECTION\n", child->Attribute("id"));
+						// prints some of the data
+						printf("    - Material id: '%s' \n", noderef->FirstChildElement("material")->Attribute("id"));
+						printf("    - Texture id: '%s' \n", noderef->FirstChildElement("texture")->Attribute("id"));
 
-                }
-                if (strcmp(child->Value(), "Leaf") == 0)
-                {
-                    // access leaf data by searching for its id in the leaves section
-                    TiXmlElement *leaf = findChildByAttribute(leavesElement, "id", child->Attribute("id"));
+						// repeat for other leaf details
+					} else
+						printf("  - Node id: '%s': NOT FOUND IN THE NODES SECTION\n", child->Attribute("id"));
 
-                    if (leaf)
-                    {
-                        // it is a leaf and it is present in the leaves section
-                        printf("  - Leaf id: '%s' ; type: '%s'\n", child->Attribute("id"), leaf->Attribute("type"));
+				}
+				if (strcmp(child->Value(), "Leaf") == 0) {
+					// access leaf data by searching for its id in the leaves section
+					TiXmlElement *leaf = findChildByAttribute(leavesElement, "id", child->Attribute("id"));
 
-                        // repeat for other leaf details
-                    }
-                    else
-                        printf("  - Leaf id: '%s' - NOT FOUND IN THE LEAVES SECTION\n", child->Attribute("id"));
-                }
+					if (leaf) {
+						// it is a leaf and it is present in the leaves section
+						printf("  - Leaf id: '%s' ; type: '%s'\n", child->Attribute("id"), leaf->Attribute("type"));
 
-                child = child->NextSiblingElement();
-            }
-            node = node->NextSiblingElement();
-        }
-    }
+						// repeat for other leaf details
+					} else
+						printf("  - Leaf id: '%s' - NOT FOUND IN THE LEAVES SECTION\n", child->Attribute("id"));
+				}
+
+				child = child->NextSiblingElement();
+			}
+			node = node->NextSiblingElement();
+		}*/
 
 }
 
 XMLScene::~XMLScene()
 {
-    delete(doc);
+	delete(doc);
 }
 
 //-------------------------------------------------------
 
 TiXmlElement *XMLScene::findChildByAttribute(TiXmlElement *parent, const char * attr, const char *val)
-// Searches within descendants of a parent for a node that has an attribute _attr_ (e.g. an id) with the value _val_
-// A more elaborate version of this would rely on XPath expressions
+	// Searches within descendants of a parent for a node that has an attribute _attr_ (e.g. an id) with the value _val_
+	// A more elaborate version of this would rely on XPath expressions
 {
-    TiXmlElement *child = parent->FirstChildElement();
-    int found = 0;
+	TiXmlElement *child = parent->FirstChildElement();
+	int found = 0;
 
-    while (child && !found)
-        if (child->Attribute(attr) && strcmp(child->Attribute(attr), val) == 0)
-            found = 1;
-        else
-            child = child->NextSiblingElement();
+	while (child && !found)
+		if (child->Attribute(attr) && strcmp(child->Attribute(attr), val) == 0)
+			found = 1;
+		else
+			child = child->NextSiblingElement();
 
-    return child;
+	return child;
 }
+
+
+
+
