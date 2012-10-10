0734d04fe9fffbd216250da70176a31f19697ac6
Cristiano Rodrigues <cristianoalexandre@gmail.com>
2012-10-10 11:53:57 +0100

    + Completa a classe CameraValues (excepto apply()).

--- CameraValues.cpp
+++ CameraValues.cpp
@@ -2,20 +2,203 @@
 
 CameraValues::CameraValues()
 {
-    
+
+
+    /* <cameras initial="cam1" >
+                <perspective id="cam1" near="0.1" far="0.2" angle="180">
+                        <from x="30" y="30" z="30" />
+                        <to x="0" y="0" z="0" />
+                </perspective>
+                <ortho id="cam2" near="0.1" far="0.2" left="20" right="20" top="20" bottom="20" />
+        </cameras> */
+    is_initial = false;
+
+    id = "cam1";
+    camera_type = PERSPECTIVE;
+    perspective.push_back(0.1);
+    perspective.push_back(0.2);
+    perspective.push_back(180);
+
+    from.push_back(30);
+    from.push_back(30);
+    from.push_back(30);
+
+    to.push_back(0);
+    to.push_back(0);
+    to.push_back(0);
+
+    ortho.push_back(0.1);
+    ortho.push_back(0.2);
+    ortho.push_back(20);
+    ortho.push_back(20);
+    ortho.push_back(20);
+    ortho.push_back(20);
+
 }
 
 CameraValues::~CameraValues()
 {
-    
+
+}
+
+void CameraValues::addPerspectiveValues(char* attribute, char* value)
+{
+    if (!strcmp("id", attribute))
+        setID(value);
+    else
+    {
+        double valued = atof(value);
+
+        if (!strcmp("near", attribute))
+            setPerspectiveNear(valued);
+        else if (!strcmp("far", attribute))
+            setPerspectiveFar(valued);
+        else if (!strcmp("angle", attribute))
+            setPerspectiveAngle(valued);
+    }
+
+}
+
+void CameraValues::addFromValues(char* attribute, char* value)
+{
+    double valued = atof(value);
+
+    if (!strcmp("x", attribute))
+        setFromX(valued);
+    else if (!strcmp("y", attribute))
+        setFromY(valued);
+    else if (!strcmp("z", attribute))
+        setFromZ(valued);
+}
+
+void CameraValues::addToValues(char* attribute, char* value)
+{
+    double valued = atof(value);
+
+    if (!strcmp("x", attribute))
+        setToX(valued);
+    else if (!strcmp("y", attribute))
+        setToY(valued);
+    else if (!strcmp("z", attribute))
+        setToZ(valued);
+}
+
+void CameraValues::addOrthoValues(char* attribute, char* value)
+{
+    double valued = atof(value);
+
+    if (!strcmp("id", attribute))
+        setID(value);
+    else
+    {
+        if (!strcmp("near", attribute))
+            setOrthoNear(valued);
+        else if (!strcmp("far", attribute))
+            setOrthoFar(valued);
+        else if (!strcmp("left", attribute))
+            setOrthoLeft(valued);
+        else if (!strcmp("right", attribute))
+            setOrthoRight(valued);
+        else if (!strcmp("near", attribute))
+            setOrthoNear(valued);
+        else if (!strcmp("near", attribute))
+            setOrthoNear(valued);
+    }
+}
+
+void CameraValues::setID(char* newID)
+{
+    id = string(newID);
+}
+
+void CameraValues::setPerspectiveNear(double newNear)
+{
+    perspective[P_NEAR] = newNear;
+}
+
+void CameraValues::setPerspectiveFar(double newFar)
+{
+    perspective[P_FAR] = newFar;
+}
+
+void CameraValues::setPerspectiveAngle(double newAngle)
+{
+    perspective[ANGLE] = newAngle;
+}
+
+void CameraValues::setFromX(double newX)
+{
+    from[X] = newX;
+}
+
+void CameraValues::setFromY(double newY)
+{
+    from[Y] = newY;
+}
+
+void CameraValues::setFromZ(double newZ)
+{
+    from[Z] = newZ;
+}
+
+void CameraValues::setToX(double newX)
+{
+    to[X] = newX;
+}
+
+void CameraValues::setToY(double newY)
+{
+    to[Y] = newY;
+}
+
+void CameraValues::setToZ(double newZ)
+{
+    to[Z] = newZ;
+}
+
+void CameraValues::setOrthoNear(double newNear)
+{
+    ortho[O_NEAR] = newNear;
+}
+
+void CameraValues::setOrthoFar(double newFar)
+{
+    ortho[O_FAR] = newFar;
+}
+
+void CameraValues::setOrthoLeft(double newLeft)
+{
+    ortho[LEFT] = newLeft;
+}
+
+void CameraValues::setOrthoRight(double newRight)
+{
+    ortho[RIGHT] = newRight;
+}
+
+void CameraValues::setOrthoTop(double newTop)
+{
+    ortho[TOP] = newTop;
+}
+
+void CameraValues::setOrthoBottom(double newBottom)
+{
+    ortho[BOTTOM] = newBottom;
 }
 
 void CameraValues::apply()
 {
-    
+
 }
 
 void CameraValues::addValues(char* element, char* attribute, char* value)
 {
-    
+    if (!strcmp("perspective", element))
+        addPerspectiveValues(attribute, value);
+    else if (!strcmp("from", element))
+        addFromValues(attribute, value);
+    else if (!strcmp("to", element))
+        addToValues(attribute, value);
+    else if (!strcmp("ortho", element))
+        addOrthoValues(attribute, value);
 }
--- CameraValues.h
+++ CameraValues.h
@@ -1,15 +1,81 @@
 #ifndef CAMERAVALUES_H
 #define	CAMERAVALUES_H
 
+#include <vector>
+#include <string>
+#include <cstdlib>
+#include <cstring>
+
 #include "SceneValues.h"
+
+using namespace std;
+
+/* ======== VECTOR POSITIONS ========= */
+/* Perspective */
+const unsigned int P_NEAR = 0;
+const unsigned int P_FAR = 1;
+const unsigned int ANGLE = 0;
+
+/* From / To */
+const unsigned int X = 0;
+const unsigned int Y = 0;
+const unsigned int Z = 0;
+
+/* Ortho */
+const unsigned int O_NEAR = 0;
+const unsigned int O_FAR = 1;
+const unsigned int LEFT = 2;
+const unsigned int RIGHT = 3;
+const unsigned int TOP = 4;
+const unsigned int BOTTOM = 5;
+
+/* ======= CAMERA TYPE ======== */
+
+const unsigned int ORTHO = 0;
+const unsigned int PERSPECTIVE = 1;
 
 class CameraValues : public SceneValues
 {
 private:
+    bool is_initial;
+    
+    string id;
+    unsigned int camera_type;
+    vector <double> perspective; /* near, far, angle */
+    vector <double> from; /* x, y, z */
+    vector <double> to; /* x, y, z */
+    vector <double> ortho; /* near, far, left, right, top, bottom*/
 
 public:
     CameraValues();
     virtual ~CameraValues();
+    
+    void addPerspectiveValues(char* attribute, char* value);
+    void addFromValues(char* attribute, char* value);
+    void addToValues(char* attribute, char* value);
+    void addOrthoValues(char* attribute, char* value);
+
+    void setID(char* newID);
+    
+    void setPerspectiveNear(double newNear);
+    void setPerspectiveFar(double newFar);
+    void setPerspectiveAngle(double newAngle);
+
+    void setFromX(double newX);
+    void setFromY(double newY);
+    void setFromZ(double newZ);
+    
+    void setToX(double newX);
+    void setToY(double newY);
+    void setToZ(double newZ);
+
+    void setOrthoNear(double newNear);
+    void setOrthoFar(double newFar);
+    void setOrthoLeft(double newLeft);
+    void setOrthoRight(double newRight);
+    void setOrthoTop(double newTop);
+    void setOrthoBottom(double newBottom);
+
     void apply();
     void addValues(char* element, char* attribute, char* value);
 };
